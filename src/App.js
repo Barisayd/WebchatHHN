5f43d97a016a6f039485e918d6816e0e2c537e6b
naydogan <naydogan@stud.hs-heilbronn.de>
2023-04-10 01:11:10 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css'
+//import { Button } from './components/Floatingbutton';
+
+
+
+import ActionProvider from './Chatbot/ActionProvider';
+import config from './Chatbot/config';
+import MessageParser from './Chatbot/MessageParser';
+
+
 
 function App() {
+
+  const [showBot, toggleBot] = useState(false);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showBot && (
+      <Chatbot config={config} messageParser={MessageParser} actionProvider={ActionProvider}/>
+     
+      )
+      }
+
+  <button onClick={() => toggleBot((prev) => !prev)}>Bot</button>
+   
     </div>
+    
   );
 }
 
---
+++ src/components/Floatingbutton.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+// add iframe to host button offpage
+export class Button extends Component {
+  render(){
+  return(
+
+  <div className='popup'>
+
+    
+          <button onClick>Click</button>
+
+  </div>
+
+  
+  )
+  }
+}
