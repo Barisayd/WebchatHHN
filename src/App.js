4122e585f5260aa54e634bbab6c53ac96fcff577
Baris Aydogan <naydogan@stud.hs-heilbronn.de>
2023-04-27 19:37:41 +0200

    #15 implemented input validator to prevent empty messages #16 implemented new custom bot avatar

--- src/App.js
+++ src/App.js
@@ -11,44 +11,54 @@ import MessageParser from './Chatbot/MessageParser';
 
 import { Fab } from '@mui/material';
 
+
 import LiveHelp from '@mui/icons-material/LiveHelp';
+import SmartToyIcon from '@mui/icons-material/SmartToy';
+
+import {ReactComponent as BotIcon} from './Icons/BotIcon.svg'
+import Icon from "@material-ui/core/Icon";
+import { fontSize } from '@mui/system';
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+
+//<svgIcon style={{fontSize: "250%"}} />
+
+//<Fab  color='warning' aria-label = "menu icon" onClick={() => toggleBot((prev) => !prev)}>
 
 function App() {
   const [showBot, toggleBot] = useState(false);
 
-  return (
+  // Prevent empty user input
+  const validator = (input) => {
+    if (input !== "" && input !== " ") return true;
+    return false
+}
 
+// Toggle Bot with custom FloatingActionButton
+  return (
     <div className="App" style={{
       position: 'absolute',
-  
   }}>
-
     <div style={{
       position: 'fixed',
       right: '6%',
       bottom: '5%'
   }}>
     {showBot && (
-      <Chatbot config={config} messageParser={MessageParser} actionProvider={ActionProvider}/>
-    
-      )
-      }
+      <Chatbot config={config} messageParser={MessageParser} actionProvider={ActionProvider} validator={validator}/>
+      )}
     </div>
       <div style={{
            width: 'fit-content',
            right: '1%',
            position: 'fixed',
            bottom: '1%'
-
       }}>
-      <Fab  color='warning' aria-label = "menu icon" onClick={() => toggleBot((prev) => !prev)}>
-      <LiveHelp />
+      <Fab size='large' onClick={() => toggleBot((prev) => !prev)}>
+    <BotIcon/>
       </Fab>
-
       </div>
-
     </div>
-
   );
 }
 
--- src/Chatbot/config.js
+++ src/Chatbot/config.js
@@ -1,11 +1,17 @@
 
 // Config starter code
 import { createChatBotMessage } from "react-chatbot-kit";
-
+import {ReactComponent as BotAvatar} from '../Icons/BotAvatar.svg'
 
 const config = {
   initialMessages: [createChatBotMessage(`Guten Tag, wie kann ich Dir behilflich sein?`)],
-  botName: 'Webchat HHN'
+  botName: 'Webchat HHN',
+  customComponents: {
+   // Replaces the default bot avatar
+   botAvatar: (props) => <BotAvatar height={50} width={50} left={50} {...props} />,
+
+ },
+  
 }
 
 export default config
