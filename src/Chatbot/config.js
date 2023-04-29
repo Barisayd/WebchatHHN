6dd6e6bc55b89f1d1467d75bd04a45fa2b1d6409
Baris Aydogan <naydogan@stud.hs-heilbronn.de>
2023-04-29 21:27:03 +0200

    added itialMessage

--- src/Chatbot/config.js
+++ src/Chatbot/config.js
@@ -4,18 +4,13 @@ import { createChatBotMessage } from "react-chatbot-kit";
 import {ReactComponent as BotAvatar} from '../Icons/BotAvatar.svg'
 
 const config = {
-<<<<<<< src/Chatbot/config.js
+  initialMessages: [createChatBotMessage(`Hi, how can I help you?`)],
   botName: 'Webchat HHN',
   customComponents: {
    // Replaces the default bot avatar
    botAvatar: (props) => <BotAvatar height={50} width={50} left={50} {...props} />,
-
  },
   
-=======
-  initialMessages: [createChatBotMessage(`Hi, how can I help you?`)],
-  botName: 'Webchat HHN'
->>>>>>> src/Chatbot/config.js
 }
 
 export default config
